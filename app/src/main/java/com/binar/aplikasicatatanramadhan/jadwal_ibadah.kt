671a0d6afc3d40a8546b6476dfe49d9052704ca7
rizqifakhri1 <rizqifakhri@gmail.com>
2022-04-04 22:15:18 +0700

    Polishing Design User Interface Jadwal Ibadah

--- app/src/main/java/com/binar/aplikasicatatanramadhan/jadwal_ibadah.kt
+++ app/src/main/java/com/binar/aplikasicatatanramadhan/jadwal_ibadah.kt
@@ -5,15 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.binar.aplikasicatatanramadhan.databinding.FragmentJadwalIbadahBinding
 
 class jadwal_ibadah : Fragment() {
+
+    private lateinit var binding:FragmentJadwalIbadahBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_jadwal_ibadah, container, false)
+        binding = FragmentJadwalIbadahBinding.inflate(layoutInflater)
+        return (binding.root)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //binding
+        binding.ibBack.setOnClickListener(){
+            findNavController().navigate(R.id.action_jadwal_ibadah_to_homeScreenFragment)
+        }
     }
 
 }
